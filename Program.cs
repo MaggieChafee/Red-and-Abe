ffa95c9c23de2654c59efe61cf9832a405265a22
MaggieChafee <maggie.chafee@gmail.com>
2024-01-09 21:18:24 -0600

    functionality there

--- Program.cs
+++ Program.cs
@@ -77,12 +77,23 @@ List<Product> products = new List<Product>()
 void ViewProducts()
 {
     Console.WriteLine("All Products:");
-    foreach (Product product in products)
+    for (int i = 0; i < products.Count; i ++)
     {
-        Console.WriteLine(@$"   -- {product.Name} --
-        Cost: {product.Price} USD 
-        TypeId: {product.TypeId}
-        Availablity: {(product.Available ? "Unavailable" : "Available")}");
+        Console.WriteLine(@$"   -- {i + 1} {products[i].Name} --
+        Cost: {products[i].Price} USD 
+        TypeId: {products[i].TypeId}
+        Availablity: {(products[i].Available ? "Unavailable" : "Available")}");
+    }
+}
+
+// Listed Products
+
+void ListProducts()
+{
+    Console.WriteLine("All Products");
+    for (int i = 0; i < products.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {products[i].Name}");
     }
 }
 
@@ -125,60 +136,198 @@ Enter New Product Name:");
     products.Add(createProduct);
     Console.WriteLine("Your product has been added!");
 }
-// DeleteProduct()
-// UpdateProduct()
-// SearchProductTYP()
+// Delete a Product
+
+void DeleteProduct()
+{
+    Console.WriteLine(@" -- DELETE A PRODUCT -- 
+    Choose a product to delete:");
+    ListProducts();
+
+    Product chosen = null;
+    
+    while (chosen == null) 
+    {
+        Console.WriteLine("Please enter the product number you wish to delete:");
+        try
+        {
+            int response = int.Parse(Console.ReadLine().Trim());
+            chosen = products[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please type only integars!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please choose an existing item only!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Please follow the prompt!");
+        }
+
+    }
+    Console.WriteLine($@"Are you sure you want to delete {chosen.Name}?
+                        1. Yes
+                        2. No");
+    string choice = null;
+
+    choice = Console.ReadLine();
+
+    if (choice == "1")
+    {
+        products.Remove(chosen);
+        Console.WriteLine($"{chosen.Name} has been deleted.");
+    }
+    else if (choice == "2")
+    {
+        Console.WriteLine($"Nevermind!");
+    }
+    else
+    {
+        Console.WriteLine($"Invalid Entry");
+    }
+}
+// Update Product
+
+void UpdateProduct()
+{
+    Console.WriteLine(@" -- EDIT PRODUCT --
+    Choose a product to edit:");
+
+    ListProducts();
+
+    Product chosen = null;
+
+    while (chosen == null)
+    {
+        Console.WriteLine("Please enter the product number you wish to edit:");
+        try
+        {
+            int response = int.Parse(Console.ReadLine().Trim());
+            chosen = products[response - 1];
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please type only integars!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please choose an existing item only!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            Console.WriteLine("Please follow the prompt!");
+        }
+    }
+
+    Console.WriteLine($@"Which would you like to edit?
+                        1. Name: {chosen.Name}
+                        2. Price: {chosen.Price}
+                        3. Availablity: {chosen.Available}
+                        4. Type Id: {chosen.TypeId}");
+
+    string answer = "";
+    decimal priceAnswer = 0M;
+    bool availAnswer = true;
+    int idAnswer = 0;
+
+    if (answer == "1")
+    {
+        Console.WriteLine("Please enter the updated name:");
+        answer = Console.ReadLine();
+        chosen.Name = answer;
+        Console.WriteLine("Your product name has been updated!");
+    } else if (answer == "2")
+    {
+        Console.WriteLine("Please enter the updated price:");
+        priceAnswer = Convert.ToDecimal(Console.ReadLine());
+        chosen.Price = priceAnswer;
+        Console.WriteLine("Your product availability has been updated!");
+    } else if (answer == "3")
+    {
+        Console.WriteLine("Please enter the updated availability (true / false):");
+        availAnswer = Convert.ToBoolean(Console.ReadLine().ToLower().Trim());
+        chosen.Available = availAnswer;
+        Console.WriteLine("Your product availability has been updated!");
+    } else if (answer == "4")
+    {
+        Console.WriteLine("Please choose an updated type Id:");
+        ViewProductTypeId();
+        idAnswer = Convert.ToInt32(Console.ReadLine());
+        chosen.Price = idAnswer;
+        Console.WriteLine("Your product type Id has been updated!");
+    }
+}
+
+// SearchProductType()
+
+void SearchProductType()
+{
+    Console.WriteLine("Enter the TypeId you would like to see:");
+    ViewProductTypeId();
+
+    int response = int.Parse(Console.ReadLine());
+
+    List<Product> filteredProducts = products.FindAll(x => x.Id == response);
+
+    foreach (Product product in filteredProducts)
+    {
+        Console.WriteLine($"{product.Name}");
+    }
+}
 
 // APP
 
 // Greeting and Menu
-string greeting = @"----Welcome to Reductio and Absurdum!----
+
+string greeting = @"---- Welcome to Reductio and Absurdum! ----
 Providers of High-Quality Magical Supplies";
+string choice = null;
 
 Console.WriteLine(greeting);
-string choice = null;
 while (choice != "0")
-{
-    Console.WriteLine(@"Choose an option to continue:
+    {
+        Console.WriteLine(@"Choose an option to continue:
             0. Exit
             1. View All Products
             2. Add a Product
             3. Delete a Product
-            4. Update a Product
+            4. Edit a Product
             5. Search for a Product
             6. View all Available Products");
 
-    choice = Console.ReadLine();
-    if (choice == "0")
-    {
-        Console.WriteLine("Goodbye!");
+        choice = Console.ReadLine();
+        if (choice == "0")
+        {
+            Console.WriteLine("Goodbye!");
+        }
+        else if (choice == "1")
+        {
+            ViewProducts();
+        }
+        else if (choice == "2")
+        {
+            AddProduct();
+        }
+        else if (choice == "3")
+        {
+            DeleteProduct();
+        }
+        else if (choice == "4")
+        {
+            UpdateProduct();
+        }
+        else if (choice == "5")
+        {
+            SearchProductType();
+        }
+        else if (choice == "6")
+        {
+            Console.WriteLine("View Available Products");
+            // ViewAvailableProducts();
+        }
     }
-    else if (choice == "1")
-    {
-        ViewProducts();
-    }
-    else if (choice == "2")
-    {
-        AddProduct();
-    }
-    else if (choice == "3")
-    {
-        Console.WriteLine("Delete a Product");
-        // DeleteProduct();
-    }
-    else if (choice == "4")
-    {
-        Console.WriteLine("Update a Product");
-        // UpdateProduct();
-    }
-    else if (choice == "5")
-    {
-        Console.WriteLine("Search Products");
-        // SearchProductType();
-    }
-    else if (choice == "6")
-    {
-        Console.WriteLine("View Available Products");
-        // ViewAvailableProducts();
-    }
-}
